35572721fdafef47b9a804dab2b08c6936c33daa
eaglesakura <eagle.sakura@gmail.com>
2016-07-20 12:22:46 +0900

    リソースファイル名を適宜変換する機能を追加

--- src/test/groovy/com/eaglesakura/android/mipmap/ImageItemTest.groovy
+++ src/test/groovy/com/eaglesakura/android/mipmap/ImageItemTest.groovy
@@ -6,12 +6,20 @@ import com.eaglesakura.tool.log.Logger;
 
 public class ImageItemTest extends GroovyTestCase {
 
+    public void test_リソース名として有効な名前に変換する() throws Throwable {
+        assertEquals(ImageItem.toResourceName("Item-00.png"), "item_00.png");
+    }
+
     public void test_画像の幅高さが取得できる() throws Exception {
         def src = new File("src/test/assets/mipmap/xxxhdpi/ic_menu_cyclecomputer.png");
         def dst = new File("src/test/gen/res");
         ToolsPath path = new ToolsPath();
+        path.identify = System.getenv("IMGMAGICK_IDENTIFY");
+        path.convert = System.getenv("IMGMAGICK_CONVERT");
 
-        Logger.out("Path = %s", src.absolutePath)
+        Logger.out("src= %s", src.absolutePath)
+        Logger.out("identify= %s", path.identify)
+        Logger.out("convert= %s", path.convert)
         assertTrue(src.file);
 
         ImageItem item = new ImageItem(src, dst);
